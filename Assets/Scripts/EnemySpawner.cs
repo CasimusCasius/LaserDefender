72934c8ccad23ffd5bce61a6a3af8d0d858e5473
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-03-03 23:58:08 +0100

    U6P9 WaveConfig for Path And Speed

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -31,8 +31,10 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
         {
-            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypointsFromPath()[0].transform.position, Quaternion.identity);
-
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), 
+                waveConfig.GetWaypointsFromPath()[0].transform.position, 
+                Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
         }
     }

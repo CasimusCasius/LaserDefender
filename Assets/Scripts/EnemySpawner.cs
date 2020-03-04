ab78e74ea8f289bfc9ca0f0566e3b86a7dd70c6f
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-03-04 14:22:14 +0100

    U6P10 Spawn multiple Enemy Waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,26 +7,19 @@ public class EnemySpawner : MonoBehaviour
 {
 
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
-
-    }
-
-   
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        StartCoroutine(SpawnAllWaves());
+
     }
 
-    IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
 
     {
         for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
@@ -34,8 +27,20 @@ public class EnemySpawner : MonoBehaviour
             var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), 
                 waveConfig.GetWaypointsFromPath()[0].transform.position, 
                 Quaternion.identity);
+            
             newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex= startingWave; waveIndex < waveConfigs.Count; waveIndex++ )
+        {
+            var currentWave = waveConfigs[waveIndex];
+
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
         }
     }
 }

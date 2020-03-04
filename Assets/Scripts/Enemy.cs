225d06038ecf65e77f4359f6f82badb47230aa71
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-03-04 20:07:23 +0100

    U6P13 Make Enemy Shoot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 1f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float projectalSpeed = 10f;
 
+
+
+    [SerializeField] GameObject enemyLaser;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter<=0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser=Instantiate(enemyLaser, transform.position , Quaternion.identity)as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectalSpeed); 
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
